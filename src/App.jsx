ac0694817738e159ee953021c756ccdc66f30b9d
VedantSinngh <vedaantsinngh@gmail.com>
2024-10-28 02:04:57 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
-        {/* <About /> */}
+        <About />
         <Technologies />
         <Experience />
         <Project />
